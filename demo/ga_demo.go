adb35e20d8d0ac904cb7cdc965a47c729d0cb36c
mh-cbon <mh-cbon@users.noreply.github.com>
2017-03-17 01:39:29 +0100

    demo: refactor

--- demo/ga_demo.go
+++ demo/ga_demo.go
@@ -10,6 +10,7 @@ import (
 // provide it a reporter such as Gh
 
 func main() {
+	// Don t forget to change the GA ID.
 	reportpanic.HandleMain(reportpanic.Ga("UA-93911415-1", "my-program", "0.0.1"), func() {
 		panic("oh no!")
 	})
